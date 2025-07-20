49b34f853a1e0d75c08d66e0fe6be793bae1ade4
LALA TECH <olaniyanshamsudeenyinka001@gmail.com>
2025-07-20 22:02:21 +0100

    first commit

---
+++ components/Carousel.jsx
@@ -0,0 +1,3 @@
+export default function Carousel() {
+  return <div className="text-2xl font-bold">Carousel Content</div>
+}
---
+++ components/Poppin.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Poppin = () => {
+  return (
+    <div>Poppin</div>
+  )
+}
+
+export default Poppin
